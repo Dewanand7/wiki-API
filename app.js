bb0041a87e5c101ab607480ec6b20e7ff1708489
Dewanand khilar <dewanandkhilar@gmail.com>
2022-01-27 23:11:02 +0530

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const req = require("express/lib/request");
 
 const app = express();
 
@@ -14,25 +15,46 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true
+});
 
 const articleSchema = {
-    title: String,
-    content: String
+  // create our schema
+  title: String,
+  content: String
 };
 
-const Article = mongoose.model("Article", articleSchema);
+const Article = mongoose.model("Article", articleSchema); // create our article model
 
-app.get("/articles", (req, res) =>{
-Article.find(function(err, foundArticles){
-    if(!err){
-        res.send(foundArticles);
+// implement chainable route handlers with app.route:
+
+
+app.get("/articles", function(req, res){
+  Article.find(function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
     } else {
-        res.send(err);
+      res.send(err);
     }
-})
+  });
+});
+
+app.post("/articles", function (req, res) {
+  // our post request targets the /articles route
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    // creates a new article using the data submitted through the post request
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  newArticle.save();
 })
 
-app.listen(3000, function() {
+
+app.listen(3000, function () {
   console.log("http://localhost:3000");
 });
